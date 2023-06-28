dd9243b05d7d4f8115d58cccb2b0b1660faf7fe3
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-06-29 00:12:18 +0530

    -added files
    -added components, utils, and others

---
+++ portfolio/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from 'page.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
---
+++ portfolio/src/app/components/Card/card.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from 'card.module.css'
+
+export default function Card() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}

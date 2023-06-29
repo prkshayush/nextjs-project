6c2a96ed8fd157d17c9bdcc14eb436d08dc8bf7e
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-06-29 20:32:10 +0530

    -updated card component
    -updated card component with all description and styling

--- portfolio/src/app/components/Navbar/navbar.tsx
+++ portfolio/src/app/components/Navbar/navbar.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
     <div>
-      
+      <Link href="/contact" />
     </div>
   )
 }
--- portfolio/src/app/contact/page.tsx
+++ portfolio/src/app/contact/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import styles from 'page.module.css'
+import styles from './page.module.css'
+import Card from '../components/Card/card'
 
 export default function Contact() {
   return (
     <div className={styles.container}>
-      
+      <Card />
     </div>
   )
 }

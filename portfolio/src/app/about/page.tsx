96fefb5b19dae870f32a93fdf180f5239b831421
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-07-07 05:42:29 +0530

    small changes
    user interface upgrades

--- portfolio/src/app/about/page.tsx
+++ portfolio/src/app/about/page.tsx
@@ -6,28 +6,28 @@ import Button from '../components/button/Button'
 
 export default function About() {
   return (
-      <div className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image src='/about-img.png' alt='' width={600} height={400} className={styles.img} />
-          <Image src='/rotate.png' alt='alt-img' width={180} height={180} className={styles.rotate} />
-        </div>
-        <div className={styles.text}>
-          <h1 className={styles.item}>About Me</h1>
-          <h3>Hi, I'm Ayush,<span className={styles.title}> Creative and innovative thinker who is always looking for new ways to use technology</span></h3>
-          <p className={styles.desc}><span className={styles.description}>As a Junior Developer, I possess an impressive arsenal of skills in JS, Python, Go & Solidity.</span>
-            <br /> Additionally, I excel in Next, React with advanced animation styles for designing and maintaining responsive websites that offer a smooth user experience.<Link href='/projects'><span className={styles.project}> Check out my projects here. </span></Link>
-             Apart from all this, I've also won various competitons in my high school one of which involved building an Arduino-based robot, an AoC bridge, etc...
-          </p>
-          <div className={styles.education}>
-            <h1 className={styles.item}>Education</h1>
-            <h3 className={styles.title}>Bachelor of Technology</h3>
-            <h3 className={styles.description}>2021-2025 | BIT Mesra, India</h3>
-            <p className={styles.desc}>Pursuing a degree with majors in Chemical Engineering & minors in Python
-            </p>
-          </div>
-        <Button url='/' text='Resume' />
-        </div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src='/about-img.png' alt='' width={600} height={400} className={styles.img} />
+        <div className={styles.rotate}><Image src='/rotate-dark.png' alt='alt-img' width={180} height={180} /></div>
       </div>
+      <div className={styles.text}>
+        <h1 className={styles.item}>About Me</h1>
+        <h3>Hi, I'm Ayush,<span className={styles.title}> Creative and innovative thinker who is always looking for new ways to use technology</span></h3>
+        <p className={styles.desc}><span className={styles.description}>As a Junior Developer, I possess an impressive arsenal of skills in JS, Python, Go & Solidity.</span>
+          <br /> Additionally, I excel in Next, React with advanced animation styles for designing and maintaining responsive websites that offer a smooth user experience.<Link href='/projects'><span className={styles.project}> Check out my projects here. </span></Link>
+          Apart from all this, I've also won various competitons in my high school one of which involved building an Arduino-based robot, an AoC bridge, etc...
+        </p>
+        <div className={styles.education}>
+          <h1 className={styles.item}>Education</h1>
+          <h3 className={styles.title}>Bachelor of Technology</h3>
+          <h3 className={styles.description}>2021-2025 | BIT Mesra, India</h3>
+          <p className={styles.desc}>Pursuing a degree with majors in Chemical Engineering & minors in Python
+          </p>
+        </div>
+        <Button url='/' text='Resume' />
+      </div>
+    </div>
 
   )
 }
--- portfolio/src/app/components/card/Card.tsx
+++ portfolio/src/app/components/card/Card.tsx
@@ -5,17 +5,45 @@ import styles from './card.module.css'
 import Button from '../button/Button'
 
 export default function Card() {
+  const links = [
+    {
+      id: 1,
+      url: 'https://github.com/prkshayush',
+      img: '/github-mark-white.png'
+    },
+    {
+      id: 2,
+      url: 'https://www.linkedin.com/in/ayush-prakash-8a3320224/',
+      img: '/linkedin-dark.png'
+    },
+    {
+      id: 3,
+      url: 'https://twitter.com/maybenot_ap',
+      img: '/twitter-dark.png'
+    },
+    {
+      id: 4,
+      url: 'https://www.instagram.com/prksh_ayush/',
+      img: '/insta.png'
+    },
+  ]
   return (
     <div className={styles.container}>
       <article className={styles.profile}>
         <div className={styles.image}><Image src='/image.jpg' alt='profile-img' width={100} height={100} /></div>
         <h2 className={styles.title}>Ayush Prakash</h2>
-        <small className={styles.userHandle}>@prksh_ayush</small>
+        <Link href='https://www.threads.net/@prksh_ayush' className={styles.userHandle}>@prksh_ayush</Link>
         <div className={styles.profileButton}>
-            <Button url='/' text='Resume' />
+          <Button url='/' text='Resume' />
         </div>
         <div className={styles.profileIcon}>
-            <Link href='#'><Image src='/github-mark-white.png' alt='' width={20} height={20} /></Link> 
+          {
+            links.map(link => (
+              <Link key={link.id} href={link.url} className={styles.socials}>
+                <Image src={link.img} alt='Icon8-Pack' width={25} height={25} />
+              </Link>
+            ))
+          }
         </div>
       </article>
     </div>

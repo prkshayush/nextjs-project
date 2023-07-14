a8271e732d943b10ead758b7dfee39d999f5b03c
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-07-14 11:15:53 +0530

    menu item changes

--- portfolio/src/app/components/menu/Menu.tsx
+++ portfolio/src/app/components/menu/Menu.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Menu() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
   return (
     <div>
-      <button className="flex flex-col justify-center item-center">
-        <span className='bg-teal-400 block h-0.5 w-6 rounded-sm -translate-y-1.5 '></span>
-        <span className='bg-teal-400 block h-0.5 w-6 rounded-sm '></span>
-        <span className='bg-teal-400 block h-0.5 w-6 rounded-sm translate-y-1.5 '></span>
+      <button className="flex flex-col justify-center item-center" onClick={handleClick}>
+        <span className={`bg-teal-400 transition-all duration-300 ease-in-out block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : ' -translate-y-1'}`}></span>
+        <span className={`bg-teal-400 transition-all duration-300 ease-in-out block h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+        <span className={`bg-teal-400 transition-all duration-300 ease-in-out block h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : ' translate-y-1'}`}></span>
       </button>
     </div>
   )
